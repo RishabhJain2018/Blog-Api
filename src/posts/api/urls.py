90e1c85d1dd880cc014e944c78927ade78a53c28
RishabhJain2018 <rishabhjain2018@gmail.com>
2016-10-03 23:30:56 +0530

    initial Commit

---
+++ src/posts/api/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+
+from .views import (
+	PostListAPIView
+	)
+
+
+urlpatterns = [
+	url(r'^$', PostListAPIView.as_view(), name='list'),
+]
