e7e7b96a1d55fd7c1ce0355afa9902552e41b23b
RishabhJain2018 <rishabhjain2018@gmail.com>
2016-10-06 00:35:04 +0530

    Added Post list & post detail.

--- src/posts/api/serializers.py
+++ src/posts/api/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from posts.models import Post
 
 
-class PostSerializer(serializers.ModelSerializer):
+class PostListSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Post
 		fields = [
@@ -13,3 +13,34 @@ class PostSerializer(serializers.ModelSerializer):
 		'content',
 		'publish',
 		]
+
+
+class PostDetailSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Post
+		fields = [
+		'id',
+		'title',
+		'slug',
+		'content',
+		'publish',
+		]
+
+
+"""
+
+data={
+	"title":"Yeah Buddy",
+	"content": "New Content",
+	"publish": "2016-2-12",
+
+}
+
+new_item = PostSerializer(data=data)
+if new_item.is_valid():
+	new_item.save()
+
+else:
+	print(new_item.errors)
+
+"""
--- src/posts/api/urls.py
+++ src/posts/api/urls.py
@@ -3,10 +3,12 @@ from django.contrib import admin
 
 
 from .views import (
-	PostListAPIView
+	PostListAPIView,
+	PostDetailAPIView
 	)
 
 
 urlpatterns = [
 	url(r'^$', PostListAPIView.as_view(), name='list'),
+	url(r'^(?P<abc>[\w+]+)/$', PostDetailAPIView.as_view(), name='detail'),
 ]
